aa8e608ea1c2858fbb7e61148045be3be6869d80
Mike Gopsill <m.gopsill@gmail.com>
2018-05-01 09:18:14 +0100

    Initial Commit

---
+++ MGScrollHeader/MGScrollHeader.h
@@ -0,0 +1,19 @@
+//
+//  MGScrollHeader.h
+//  MGScrollHeader
+//
+//  Created by Mike Gopsill on 01/05/2018.
+//  Copyright © 2018 Mike Gopsill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MGScrollHeader.
+FOUNDATION_EXPORT double MGScrollHeaderVersionNumber;
+
+//! Project version string for MGScrollHeader.
+FOUNDATION_EXPORT const unsigned char MGScrollHeaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGScrollHeader/PublicHeader.h>
+
+
